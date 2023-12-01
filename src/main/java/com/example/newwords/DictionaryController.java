33cb4a36e403e810237e1671d5f0a3ee13b1c9e7
dvgiap <juuzou152@gmail.com>
2023-12-01 11:18:10 +0700

    up

--- src/main/java/com/example/newwords/DictionaryController.java
+++ src/main/java/com/example/newwords/DictionaryController.java
@@ -7,8 +7,6 @@ import java.net.URL;
 import java.util.ResourceBundle;
 
 public class DictionaryController implements Initializable {
-    public void searchWord(ActionEvent actionEvent) {
-    }
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
