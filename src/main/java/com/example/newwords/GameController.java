28b18477e693bb1b64028df469bc477016b27d1e
Duc Dang <duc.dang.bui2004@gmail.com>
2023-12-03 13:06:46 +0700

    Sửa code trong phương thức runGame của GameController để  không trả ra Exception

--- src/main/java/com/example/newwords/GameController.java
+++ src/main/java/com/example/newwords/GameController.java
@@ -1,9 +1,13 @@
 package com.example.newwords;
 
+import com.almasb.fxgl.app.FXGLPane;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.stage.Stage;
+import javafx.stage.WindowEvent;
 
 import java.net.URL;
 import java.util.ResourceBundle;
@@ -15,7 +19,15 @@ public class GameController implements Initializable {
 
     @FXML
     void runGame(ActionEvent event) {
-        Game.Main.main(new String[0]);
+        Game.Main game = new Game.Main();
+        FXGLPane gamePane = Game.Main.embeddedLaunch(game);
+        Stage stage = new Stage();
+        stage.setScene(new Scene(gamePane, 1200, 800));
+        stage.addEventHandler(WindowEvent.WINDOW_CLOSE_REQUEST, windowEvent -> {
+            Game.Main.embeddedShutdown();
+            Game.pregame.clearPreGame();
+        });
+        stage.show();
     }
 
     @Override
