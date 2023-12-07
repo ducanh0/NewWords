fea69a73b3d8eec5cb53babe8086b03dca08e446
Duc Dang <duc.dang.bui2004@gmail.com>
2023-12-07 22:20:02 +0700

    Cập nhật CSDL dùng multithreading

--- src/main/java/com/example/newwords/supportAPI/CoreGUIDictionaryManager.java
+++ src/main/java/com/example/newwords/supportAPI/CoreGUIDictionaryManager.java
@@ -7,28 +7,34 @@ import main.Word;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 public class CoreGUIDictionaryManager {
     MyDictionary dictionary;
     DatabaseManager dbm;
+    ExecutorService service;
     public CoreGUIDictionaryManager()
     {
         dictionary = new MyDictionary();
         dbm = new DatabaseManager(Main.class.getResource("copy.db").toString());
         dbm.importDictionary(dictionary);
+        service = Executors.newSingleThreadExecutor();
     }
     public void addWord(String target, List<String> explains){
         Word word = new Word(target,new ArrayList<>(explains));
         dictionary.addWord(word);
-      //  dbm.insert(word);
+        service.submit(() -> dbm.insert(word));
     }
 
     public void fixTarget(String oldTarget, String newTarget) {
         Word findWord=dictionary.findWord(oldTarget);
         Word newWord = new Word(newTarget, findWord.getWord_explain());
         dictionary.fixWord(findWord, newWord);
-//        dbm.delete(oldTarget);
-//        dbm.insert(newWord);
+        service.submit(() -> {
+            dbm.delete(oldTarget);
+            dbm.insert(newWord);
+        });
     }
 
     public void fixExplanations(String target, List<String> meaning) {
@@ -37,7 +43,7 @@ public class CoreGUIDictionaryManager {
         for (String s : meaning)
             newWord.setWord_explain(s);
         dictionary.fixWord(findWord, newWord);
-      //  dbm.update(newWord);
+        service.submit(() -> dbm.update(newWord));
     }
 
     public Word search(String target) {
@@ -46,7 +52,7 @@ public class CoreGUIDictionaryManager {
 
     public void delete(String target) {
         dictionary.removeWord(new Word(target));
-       // dbm.delete(target);
+        service.submit(() -> dbm.delete(target));
     }
 
     public ArrayList<Word> searchPrefix(String prefix) {
